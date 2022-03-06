4220acdcf3a432422880e27dbb61b63a13f23148
Jose <josejesuscedeno@gmail.com>
2022-03-06 18:45:41 +0000

    nose

--- exercises/01-alert-on-click/index.js
+++ exercises/01-alert-on-click/index.js
@@ -1,3 +1,17 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
+
+//esto es lo que he hecho hasta ahora, si no funciona, puedo eliminar de aqui hacia abajo
+let button = document.querySelector("#button2");
+button.addEventListener("click", function() {
+	let elem = document.createElement("div");
+	elem.innerHTML = "<li>"+"hola"+"</li>";
+	document.body.appendChild(elem)
+})
+
+
+//let myFunction = document.querySelector("#button2")
+//myFunction.addEventListener( e => {
+//	alert("#button2");
+//})
